7fdde20bb97dd8853a633a7dc611dedc2a60699f
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-02-28 22:03:31 +1100

    attempting to add my sql file

---
+++ dB/carseeds.sql
@@ -0,0 +1,66 @@
+
+-- Let me know if there are any issues, I will correct anything. I put everything in lowercase anyways --
+USE carInventoryDB;
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "toyota", "supra", "1998", "175000.00", "101500", "true", "true", "6", "yellow", "coupe", "a great condition toyota supra for sale offered at our dealership"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "boxster", "2022", "249990.00", "2797", "true", "false", "6", "white", "convertible", "718 982 boxster gts 4.0 convertible 2dr pdk 7sp 4.0i [my22]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "carrera", "2019", "299990.00", "9325", "true", "false", "6", "yellow", "coupe", "911 992 carrera s coupe 2dr pdk 8sp 3.0tt [my20]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "gt2", "2018", "709990.00", "5933", "true", "false", "6", "black", "coupe", "911 991 ii gt2 rs coupe 2dr pdk 7sp 3.8tt [my19]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "targa", "2015", "239990.00", "73176", "true", "false", "6", "black", "targa", "911 991 targa 4s 2dr pdk 7sp awd 3.8i [my15]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "targa", "2006", "129990.00", "93970", "true", "true", "6", "grey", "targa", "911 997 4 targa 2dr spts auto 5sp awd 3.6i [my07]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "cayenne", "2018", "193990.00", "48126", "true", "true", "8", "grey", "wagon", "9ya turbo wagon 5dr tiptronic 8sp 4x4 4.0tt [my19]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "porsche", "clubsport", "2000", "229990.00", "72928", "true", "false", "6", "silver", "coupe", "911 996 gt3 clubsport coupe 2dr man 6sp 3.6i"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "aston martin", "volante", "2011", "239990.00", "31251", "true", "true", "12", "silver", "convertible", "dbs volante 2dr touchtronic 6sp 5.9i [my11]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "aston martin", "db9", "2012", "189990.00", "29452", "true", "true", "12", "grey", "coupe", "db9 coupe 2dr sa 6sp 5.9i [my13]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "aston martin", "db11", "2017", "294990.00", "26880", "true", "true", "12", "white", "coupe", "db11 launch edition coupe 2dr spts auto 8sp 5.2tt [my17]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "bentley", "arnage", "2003", "109990.00", "58286", "true", "true", "8", "black", "sedan", "arnage series 2 t black label sedan 4dr auto 4sp 6.75tt"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "bentley", "bentayga", "2017", "259990.00", "35970", "true", "true", "8", "white", "wagon", "bentayga 4v wagon 5dr spts auto 8sp awd 4.0dttec (5-st) [my18]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "bentley", "mulsanne", "2015", "379990.00", "12755", "true", "true", "8", "onyx", "sedan", "mulsanne 3y sedan 4dr spts auto 8sp 6.8tt [my15]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "maserati", "gransport", "2018", "99990.00", "17094", "true", "true", "6", "white", "sedan", "ghibli m157 gransport sedan 4dr spts auto 8sp 3.0tt [my18]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "mclaren", "675lt", "2015", "poa", "3622", "true", "false", "8", "grey", "coupe", "2dr ssg 7sp 3.8tt [jul]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "mclaren", "p1", "2014", "2849990.00", "1228", "true", "true", "8", "yellow", "coupe", "finished in volcano yellow paintwork with a leather/alcantara interior"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "mclaren", "620r", "2021", "549990.00", "10064", "true", "false", "8", "silver", "coupe", "ma3 coupe 2dr ssg 7sp 3.8tt [my20]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "mclaren", "spider", "2019", "369990.00", "7973", "true", "true", "8", "blue", "convertible", "570s p13 spider 2dr ssg 7sp 3.8tt [my19]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "lamborghini", "aventador", "2019", "poa", "1513", "true", "true", "12", "grigio telesto", "coupe", "834 svj coupe 2dr isr 7sp awd 6.5i [my19]"),
+
+INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
+VALUES ("", "ferrari", "fiorano", "2007", "299990.00", "32792", "true", "false", "12", "rosso corse", "coupe", "599 fiorano gtb coupe 2dr seq. mac 6sp 6.0i"),
