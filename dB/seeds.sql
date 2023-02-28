15abbfe1c103b19f9857d1fa2c13205fa38c4397
Abewd <116496332+abewd@users.noreply.github.com>
2023-02-28 19:56:47 +1100

    fixed small seeds.sql issue

--- dB/seeds.sql
+++ dB/seeds.sql
@@ -1,4 +1,4 @@
 USE carInventoryDB;
 
 INSERT INTO cars (id, make, model, year, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
-VALUES ("001", "Toyota", "Supra", "1998", "175000.00", "101500", "TRUE", "TRUE", "6", "YELLOW", "COUPE", "A Great condition Toyota Supra for sale offered at our dealership"),
+VALUES ("", "Toyota", "Supra", "1998", "175000.00", "101500", "TRUE", "TRUE", "6", "YELLOW", "COUPE", "A Great condition Toyota Supra for sale offered at our dealership"),
--- routes/api/carFunctions.js
+++ routes/api/carFunctions.js
@@ -36,3 +36,39 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get("/", async (req, res) => {
+  // Display a unit by its ID value
+  try {
+    const carsOnLott = await Cars.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: [
+            "id",
+            "make",
+            "model",
+            "year",
+            "price",
+            "mileage",
+            "fossil_fuel",
+            "automatic",
+            "engine_cylinders",
+            "color",
+            "body_type",
+            "car_description",
+          ],
+        },
+      ],
+    });
+    if (!carsOnLott) {
+      res
+        .status(404)
+        .json({ message: "Bro, we cant find the ID for this box on wheels" });
+      return;
+    }
+    res.status(200).json(carsOnLott);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
