d8d5f824b28df1ac83612670c3942b3858a137c7
Marwin Manlangit <marwin.m@hotmail.com>
2023-03-01 14:26:31 +1100

    completed most of the options for sidebar

---
+++ dist/js/index.js
@@ -0,0 +1,28 @@
+// function displayCars () {
+//     // Replace Car Title with Year Make Model
+//     var carTitle = document.getElementById('car-title');
+//     carTitle.innerHTML = ;
+
+//     // Replace Car Price
+//     var carPrice = document.getElementById('price');
+//     carPrice.innerHTML = ;
+
+//     // Replace Car Description
+//     var carDescription = document.getElementById('description');
+//     carDescription.innerHTML = ;
+
+//     // Replace Odometer
+//     var carMileage = document.getElementById('mileage');
+//     carMileage.innerHTML = ;
+
+//     // Replace Transmission
+//     var carTransmission = document.getElementById('transmission');
+//     carTransmission.innerHTML = ; 
+
+//     // Replace Body Type
+//     var carBodyType = document.getElementById('body-type');
+//     carBodyType.innerHTML = ;
+
+// } 
+
+// displayCars ();
