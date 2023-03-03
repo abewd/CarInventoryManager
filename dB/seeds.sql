8300e4eddd379c60ae3db059bf8a94d135abab37
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-03 19:12:22 +1100

    deleted some useless files we dont need.

--- dB/seeds.sql
+++
@@ -1,4 +0,0 @@
-USE carInventoryDB;
-
-INSERT INTO cars (make, model, years, price, mileage, fossil_fuel, automatic, engine_cylinders, color, body_type, car_description)
-VALUES ("toyota", "supra", 1998, 175000.00, 101500, true, true, 6, "yellow", "coupe", "a great condition toyota supra for sale offered at our dealership");
