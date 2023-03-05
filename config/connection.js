77d83486c86a7ccb111d041d56faa0860dd74750
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-05 16:14:37 +1100

    added code for connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
+// Import the Sequelize library used to connect a MySql DB
 const Sequelize = require("sequelize");
+
+// Import the dotenv library for loading environment variables from a .env file
 require("dotenv").config();
 
+// Initialise a variable for the Sequelize instance
 let sequelize;
 
+// Check if the JAWSDB_URL environment variable exists
 if (process.env.JAWSDB_URL) {
+  // If it does, initialise the Sequelize
+  // instance with this variable to connect to a remote database
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // If it doesnt, then initialise the Sequelize with the local
+  // database credentials specified in the .env file
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -18,4 +27,5 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
+// Export Sequelize instance to be used in the server.js
 module.exports = sequelize;
