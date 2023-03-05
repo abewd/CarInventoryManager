b1acacedeaa28ee4c139241b64a39a16333fd1e3
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-05 19:49:21 +1100

    added comments for index.js and userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,30 +1,39 @@
+// Import router from the express framework
 const router = require("express").Router();
+
+// Import a module called User from the following directory
 const { User } = require("../../models");
 
+// Handle POST requests to the "/login" URL
 router.post("/login", async (req, res) => {
   try {
+    // Find the user with the specified email address
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
+      // If no user is found, send a JSON response with a 400 status code and an error message
       res
         .status(400)
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
 
+    // Check if the provided password matches the hashed password in the database
     const validPassword = await userData.checkPassword(req.body.password);
 
+    // If the password is invalid, send a JSON response with a 400 status code and an error message
     if (!validPassword) {
       res
         .status(400)
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
-
+    // If the user is found and the password is valid, save the user ID and logged_in flag to the session object
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
+      // Send a JSON response with the user data and a success message
       res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
@@ -32,6 +41,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// Handle POST requests to the "/logout" URL
 router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -42,4 +52,5 @@ router.post("/logout", (req, res) => {
   }
 });
 
+// Export this data so it can be used in other parts of the codebase
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 // This will link the inventory and car functions to the server
 const router = require("express").Router();
+
+// Importing withAuth middleware to assit with authentication for passwords
 const withAuth = require("../utils/auth");
+
+// Create a module called apiRoutes which uses the following routing
 const apiRoutes = require("./api");
-
 const path = require("path");
-
 router.use("/api", apiRoutes);
 
+// Handle GET requests to the "/" URL
 router.get("/", withAuth, (req, res) => {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
+// Handle GET requests to the "/login" URL
 router.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/html/login.html"));
 });
 
+// Handle GET requests to the "/contact" URL
 router.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/html/contact.html"));
 });
 
+// Export this data so it can be used in other parts of the codebase
 module.exports = router;
