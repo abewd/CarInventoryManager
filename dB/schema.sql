228998394cf0f3363baeb776c41bfefe9a28d2b4
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-05 16:18:09 +1100

    added comments for schema.sql

--- dB/schema.sql
+++ dB/schema.sql
@@ -1,26 +1,14 @@
+-- Use the carInventoryDB 
 USE carInventoryDB;
+
+-- Delete the carInventoryDB if it already exists to prevent conflicts 
 DROP DATABASE IF EXISTS carInventoryDB;
+
+-- Delete the cars and users table if it already exists to prevent conflicts 
 DROP TABLE IF EXISTS cars;
 DROP TABLE IF EXISTS user;
 
+-- Create the carInventoryDB if it does not exist
 CREATE DATABASE carInventoryDB;
 USE carInventoryDB;
 
--- CREATE TABLE user (
--- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
--- );
-
--- CREATE TABLE cars (
--- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- make VARCHAR(30) NOT NULL,
--- model VARCHAR(30) NOT NULL,
--- years INT NOT NULL,
--- price DECIMAL NOT NULL,
--- mileage INT NOT NULL,
--- fossil_fuel BOOLEAN NOT NULL,
--- automatic BOOLEAN NOT NULL,
--- engine_cylinders DECIMAL NOT NULL,
--- color VARCHAR(30) NOT NULL,
--- body_type VARCHAR(30) NOT NULL,
--- car_description TEXT NOT NULL
--- );
