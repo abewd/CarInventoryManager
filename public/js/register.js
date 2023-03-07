cfb412a6e7624de2cd7965fc685ff0cb1ee3eb44
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-08 01:31:58 +1100

    new code for register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,36 +1,54 @@
 $(document).ready(function () {
   $("#addButton").click(function () {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var confirmPassword = $("#confirm_password").val();
-    console.log("testing" + name);
-    console.log("testing" + email);
-    console.log("testing" + password);
-
-    var newUser = {
-      name: name,
-      email: email,
-      password: password,
-    };
-    console.log("testing" + newUser);
-
-    fetch("/api/user/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newUser),
-    })
-      .then(function (response) {
-        console.log("testing" + response);
-        if (response.status === 201) {
-          alert("New user has been registered");
-        } else {
-          throw new Error("Error adding new user.");
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-        alert("Error adding new user: " + error);
-      });
+      registerHandler();
   });
 });
+
+const registerHandler = async (event) => {
+  event.preventDefault();
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var confirmPassword = $("#confirm_password").val();
+
+  console.log("testing" + name);
+  console.log("testing" + email);
+  console.log("testing" + password);
+
+  if (name == "") {
+      alert("Name is empty");
+  } else if (email == "") {
+      alert("Email is empty");
+  } else if (password == "") {
+      alert("Password is empty");
+  } else if (confirmPassword == "") {
+      alert("Confirm password is empty");
+  } else if (password != confirmPassword) {
+      alert("Password and Confirm password is not matched.");
+  } else {
+      var newUser = {
+          name: name,
+          email: email,
+          password: password,
+      };
+
+      const response = await fetch("/api/user/register", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(newUser),
+      });
+      if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          if (data.status === 201) {
+              alert("New user has been registered");
+          } else {
+              throw new Error("Error adding new user.");
+          }
+      } else {
+          alert("Unable to register the user");
+      }
+  }
+}
+register.js
+2 KB
