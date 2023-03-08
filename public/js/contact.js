61c377f5634a450b39b779c1084641487a6e2dc8
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-08 14:04:09 +1100

    formatting for the contact us page

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
   $("#contactForm").submit(function (event) {
     event.preventDefault();
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
     var message = $("#message").val();
-    window.location.href = `mailto:abdullahalfadhly@gmail.com?body=${message}`;
+
+    window.location.href = `mailto:abdullahalfadhly@gmail.com?body=${firstName} ${lastName}\n${email}\n${phone}\n${message}`;
   });
 });
